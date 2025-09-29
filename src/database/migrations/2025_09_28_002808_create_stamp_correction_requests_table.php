a689bb233e2110053b79d206fd15fcf4d9c90a8a
yoko bessho <yoko.murakami.11.18@gmail.com>
2025-09-29 16:11:23 +0900

    ER図作成

---
+++ src/database/migrations/2025_09_28_002808_create_stamp_correction_requests_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStampCorrectionRequestsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stamp_correction_requests', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('attendance_id')->nullable()->constrained()->onDelete('cascade');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('type');
+            $table->foreignId('target_break_id')->nullable()->constrained('breaks')->onDelete('set null');
+            $table->string('reason');
+            $table->dateTime('revised_start_time');
+            $table->dateTime('revised_end_time');
+            $table->string('status')->default('pending');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stamp_correction_requests');
+    }
+}
